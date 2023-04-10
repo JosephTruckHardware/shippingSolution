0b5826b1d49da0fadee0c56b3c5f49f0971a9def
JosephTruckHardware <Josephtruckhardware>
2023-04-10 10:35:03 -0600

    Working, just not updating the actual DB

--- db/migrations/006_create_item_parcel_table.rb
+++ db/migrations/006_create_item_parcel_table.rb
@@ -2,10 +2,9 @@ Sequel.migration do
 	up do
 		create_table(:parcels_items) do
 			primary_key :id
-            foreign_key :parcel_id, :parcels
-            foreign_key :item_id, :items
-			foreign_key :shipment_id, :shipments
-            Integer :quantity
+			Integer :quantity
+			foreign_key :parcel_id, :parcels
+			foreign_key :item_id, :items
         end
 	end
 
